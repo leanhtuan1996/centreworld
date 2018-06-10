a45f4c6d911da32dc2b0c8c731b6f1b430e98797
Le Anh Tuan <leanhtuan110596@gmail.com>
2018-06-10 20:50:18 +0700

    fix config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 //Thông tin website
-define('SITE_URL', 'http://localhost:81/cw');	
+define('SITE_URL', 'http://localhost:8081/cw');	
 
 //Thông tin cấu hình DB
 define('DB_SERVER', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'qltttm');
 
 ?>
