66110d1593a27ffba45954e918faeaef329bab82
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-11 10:30:09 +0700

    1

--- libraries/database.php
+++ libraries/database.php
@@ -76,7 +76,8 @@ function db_insert($table, $data = array())
     // Xóa ký từ , ở cuối chuỗi
     $fields = trim($fields, ',');
     $values = trim($values, ',');
-    echo "<script type='text/javascript'>alert('$message');</script>";
+    echo "<script type='text/javascript'>alert('$fields');</script>";
+    echo "<script type='text/javascript'>alert('$values');</script>";
     // Tạo câu SQL
     $sql = "INSERT INTO {$table}($fields) VALUES ({$values})";
     // Thực hiện INSERT
