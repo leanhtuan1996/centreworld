442f9046b1cfe622a06512573593edcec9cc1501
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-11 10:34:23 +0700

    1

--- libraries/database.php
+++ libraries/database.php
@@ -73,7 +73,6 @@ function db_insert($table, $data = array())
         $values .= "'".addslashes($value)."',";
         
     }
-    echo "<script type='text/javascript'>alert('$fields');</script>";
     echo "<script type='text/javascript'>alert('$values');</script>";
      
     // Xóa ký từ , ở cuối chuỗi
