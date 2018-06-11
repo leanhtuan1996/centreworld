52b23c4d677a593589acad680904f300b8252e21
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-11 10:43:40 +0700

    1

--- fix/register.php
+++ fix/register.php
@@ -9,19 +9,19 @@
     <meta name="author" content="">
     <link rel="icon" href="../../images/favicon.ico">
 
-    <title>Super Admin - Registration </title>
+    <title>Centre World Admin - Registration </title>
   
 	<!-- Bootstrap 4.0-->
-	<link rel="stylesheet" href="assets/vendor_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/vendor_components/bootstrap/dist/css/bootstrap.min.css">
 
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="assets/vendor_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../assets/vendor_components/font-awesome/css/font-awesome.min.css">
 
 	<!-- Ionicons -->
-	<link rel="stylesheet" href="assets/vendor_components/Ionicons/css/ionicons.min.css">
+	<link rel="stylesheet" href="../assets/vendor_components/Ionicons/css/ionicons.min.css">
 
 	<!-- Theme style -->
-	<link rel="stylesheet" href="css/master_style.css">
+	<link rel="stylesheet" href="../css/master_style.css">
 
 	<!-- foxadmin Skins. Choose a skin from the css/skins
 	   folder instead of downloading all of them to reduce the load. -->
@@ -61,8 +61,7 @@ $data = array(
 	'name' => $name,
 
 );
-echo "<script type='text/javascript'>alert('$email');</script>";
-     
+
 	$error = db_user_validate($data);
 
 	if(!$error){
@@ -102,18 +101,18 @@ echo "<script type='text/javascript'>alert('$email');</script>";
         <span class="ion ion-email form-control-feedback "></span>
       </div>
       <div class="form-group has-feedback">
-      <div class="form-group"><input type="password" class="form-control" id="pwd" value="" name="password" placeholder="Mật khẩu(*)" required></div>
+      <div class="form-group"><input type="password" class="form-control" id="pwd" value="" name="password" placeholder="Mật khẩu (*)" required></div>
         <span class="ion ion-locked form-control-feedback "></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Nhập lại mật khẩu(*)">
+        <input type="password" class="form-control" placeholder="Retype password">
         <span class="ion ion-log-in form-control-feedback "></span>
       </div>
       <div class="row">
         <div class="col-12">
           <div class="checkbox">
             <input type="checkbox" id="basic_checkbox_1" >
-			<label for="basic_checkbox_1">Tôi đồng ý với các <a href="register.html#"><b>Điều khoản</b></a></label>
+			<label for="basic_checkbox_1">I agree to the <a href="register.html#"><b>Terms</b></a></label>
           </div>
         </div>
         <!-- /.col -->
@@ -126,15 +125,15 @@ echo "<script type='text/javascript'>alert('$email');</script>";
     </form>
 	
 	<div class="social-auth-links text-center">
-      <p>- Đăng nhập bằng -</p>
+      <p>- OR -</p>
       <a href="register.html#" class="btn btn-social-icon btn-circle btn-facebook"><i class="fa fa-facebook"></i></a>
       <a href="register.html#" class="btn btn-social-icon btn-circle btn-google"><i class="fa fa-google-plus"></i></a>
     </div>
 	<!-- /.social-auth-links -->
     
      <div class="margin-top-20 text-center">
-      <p>Bạn đã có tài khoản?<a href="../login.php
-      " class="text-info m-l-5">Đăng nhập</a></p>
+      <p>Already have an account?<a href="../login.php
+      " class="text-info m-l-5"> Sign In</a></p>
      </div>
     
   </div>
@@ -144,13 +143,13 @@ echo "<script type='text/javascript'>alert('$email');</script>";
 
 
 	<!-- jQuery 3 -->
-	<script src="assets/vendor_components/jquery/dist/jquery.min.js"></script>
+	<script src="../assets/vendor_components/jquery/dist/jquery.min.js"></script>
 	
 	<!-- popper -->
-	<script src="assets/vendor_components/popper/dist/popper.min.js"></script>
+	<script src="../assets/vendor_components/popper/dist/popper.min.js"></script>
 	
 	<!-- Bootstrap 4.0-->
-	<script src="assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="../assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	
 	
 </body>
