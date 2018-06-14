f5fd412a6568a8c8cee767d24732a8e7ad44b2c3
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-14 11:38:59 +0700

    1

--- models/kiot.php
+++ models/kiot.php
@@ -108,18 +108,14 @@ function db_kiot_validate($data)
 	$error = array();
 	
 	
-	if (!isset($data['name_k'])){
-		
-		$error['name_k'] = 'Tên kiot không được để trống';
-		
-	}
+	if (!($error) && isset($data['description']) && $data['description']){
+        $sql = "SELECT count(ID_k) as counter FROM kiot WHERE description'".addslashes($data['description'])."'";
+        $row = db_get_row($sql);
+        if ($row['counter'] > 0){
+            $error['description'] = 'thuơng hiệu này đã tồn tại';
+        }
+    }
 	
-	
-	if (!isset($data['price'])){
-		
-		$error['price'] = 'Giá kiot không được để trống';
-		
-	}
 	
 	return $error;
 	
@@ -145,14 +141,6 @@ function db_contract_validate($data)
 		$error['ctID'] = 'ctID không được để trống';
 		
 	}
-	if (!($error) && isset($data['description']) && $data['description']){
-        $sql = "SELECT count(ID_k) as counter FROM kiot WHERE description='".addslashes($data['description'])."'";
-        $row = db_get_row($sql);
-        if ($row['counter'] > 0){
-            $error['description'] = 'thuơng hiệu này đã tồn tại';
-        }
-    }
-	
 	return $error;
 	
 }
