20f7fe0a9e4ea52aa4a5524375a657b10991c45e
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-14 11:07:54 +0700

    1

--- fix/cms/register.php
+++ fix/cms/register.php
@@ -9,19 +9,19 @@
     <meta name="author" content="">
     <link rel="icon" href="../../images/favicon.ico">
 
-    <title>Centre World Admin - Registration </title>
+    <title>Super Admin - Registration </title>
   
 	<!-- Bootstrap 4.0-->
-	<link rel="stylesheet" href="../assets/vendor_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor_components/bootstrap/dist/css/bootstrap.min.css">
 
 	<!-- Font Awesome -->
-	<link rel="stylesheet" href="../assets/vendor_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor_components/font-awesome/css/font-awesome.min.css">
 
 	<!-- Ionicons -->
-	<link rel="stylesheet" href="../assets/vendor_components/Ionicons/css/ionicons.min.css">
+	<link rel="stylesheet" href="assets/vendor_components/Ionicons/css/ionicons.min.css">
 
 	<!-- Theme style -->
-	<link rel="stylesheet" href="../css/master_style.css">
+	<link rel="stylesheet" href="css/master_style.css">
 
 	<!-- foxadmin Skins. Choose a skin from the css/skins
 	   folder instead of downloading all of them to reduce the load. -->
@@ -36,15 +36,14 @@
 
 	<!-- google font -->
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
-<?php 
-include_once '../libraries/helper.php';
-include_once '../libraries/database.php';
-include_once '../libraries/session.php';
-include_once '../models/user.php';
-if(session_get('user')){	
-	
-	redirect('index.php');	
-}
+  <?php 
+include_once '../../libraries/helper.php';
+include_once '../../libraries/database.php';
+include_once '../../libraries/session.php';
+include_once '../../models/user.php';
+include_once '../../models/role.php';
+
+
 
 $error = array();
 
@@ -90,34 +89,26 @@ $data = array(
 <?php }  
 ?>
 
-
-    <form id="signup-form" role="form" method="POST" action="" >
+  <form action="" method="POST"enctype="multipart/form-data" class="form-horizontal row-fluid"> 
       <div class="form-group has-feedback">
-      <div class="form-group"><input type="text" class="form-control"  value="" name="name" placeholder="Họ và tên(*)" required></div>
-        <span class="ion ion-person form-control-feedback "></span>
+      <div class="form-group"><input type="text" class="form-control"   name="name"  value= "" placeholder="Họ và tên(*)" required></div>
       </div>
       <div class="form-group has-feedback">
-      <div class="form-group"><input type="text" class="form-control" value="" name="email" placeholder="Email(*)" required></div>
-        <span class="ion ion-email form-control-feedback "></span>
+      <div class="form-group"><input type="email" class="form-control" name="email"  value= "" placeholder="Email(*)" required></div>
       </div>
       <div class="form-group has-feedback">
-      <div class="form-group"><input type="password" class="form-control" id="pwd" value="" name="password" placeholder="Mật khẩu (*)" required></div>
-        <span class="ion ion-locked form-control-feedback "></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Retype password">
-        <span class="ion ion-log-in form-control-feedback "></span>
+      <div class="form-group"><input type="password" class="form-control"  name="password"  value= "" placeholder="Mật khẩu(*)" required></div>
       </div>
       <div class="row">
         <div class="col-12">
           <div class="checkbox">
             <input type="checkbox" id="basic_checkbox_1" >
-			<label for="basic_checkbox_1">I agree to the <a href="register.html#"><b>Terms</b></a></label>
+			<label for="basic_checkbox_1">Tôi đồng ý với các <a href="register.html#"><b>Điều khoản</b></a></label>
           </div>
         </div>
         <!-- /.col -->
         <div class="col-12 text-center">
-        <button class="btn btn-info btn-block btn-flat margin-top-10"  type="submit">Đăng kí</button> 
+        <button class="btn btn-info btn-block btn-flat margin-top-10"  type="submit">Đăng ký</button> 
         <input type="hidden" name="request_name" value="register"/>	
         </div>
         <!-- /.col -->
@@ -125,15 +116,15 @@ $data = array(
     </form>
 	
 	<div class="social-auth-links text-center">
-      <p>- OR -</p>
+      <p>- Đăng nhập bằng -</p>
       <a href="register.html#" class="btn btn-social-icon btn-circle btn-facebook"><i class="fa fa-facebook"></i></a>
       <a href="register.html#" class="btn btn-social-icon btn-circle btn-google"><i class="fa fa-google-plus"></i></a>
     </div>
 	<!-- /.social-auth-links -->
     
      <div class="margin-top-20 text-center">
-      <p>Already have an account?<a href="../login.php
-      " class="text-info m-l-5"> Sign In</a></p>
+      <p>Bạn đã có tài khoản?<a href="../login.php
+      " class="text-info m-l-5">Đăng nhập</a></p>
      </div>
     
   </div>
@@ -143,13 +134,13 @@ $data = array(
 
 
 	<!-- jQuery 3 -->
-	<script src="../assets/vendor_components/jquery/dist/jquery.min.js"></script>
+	<script src="assets/vendor_components/jquery/dist/jquery.min.js"></script>
 	
 	<!-- popper -->
-	<script src="../assets/vendor_components/popper/dist/popper.min.js"></script>
+	<script src="assets/vendor_components/popper/dist/popper.min.js"></script>
 	
 	<!-- Bootstrap 4.0-->
-	<script src="../assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	
 	
 </body>
