f5e0bd892c3c3019b0038b12843886bc9f0f1a33
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-17 17:34:09 +0700

    1

--- modules/common/info.php
+++ modules/common/info.php
@@ -55,25 +55,18 @@ if(session_get('user')){
 		'id' => $id,
    
 		);
-		resetUser($pass);
-		 echo '<script>'
+		echo "(<script>"	
 		$(function(){
-		  $("#first, #second").on("keyup", function () {
-			var fst=$("#first").val();
-			var sec=$("#second").val();
-			if (Number(sec)!=Number(fst)) {
-			  alert("Second value should less than first value");
-			return true;
-			}
-			// else
-			// (Number(sec)<Number(fst)) {
-			//   alert("first value should less than second value");
-			// return true;
-			// }
+			$("#first, #second").on("keyup", function () {
+			  var fst=$("#first").val();
+			  var sec=$("#second").val();
+			  if (Number(sec)!=Number(fst)) {
+				alert("Second value should less than first value");
+			  return true;
+			  }
+		echo "</script>"
+		resetUser($pass);
 		
-		  })
-		})
-		echo '</script>'	
 		echo '<script language="javascript">';
 		echo 'alert("Thay đổi mật khẩu thành công")';
 		echo '</script>';
