e8e5b8cebbb969e919ee9a8ccd0cf64f9d6d3f41
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-19 12:42:26 +0700

    1

--- admin/register.php
+++ admin/register.php
@@ -105,7 +105,7 @@ $data = array(
 						<div class="help-block"></div></div>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name = "password2"data-validation-match-match="password"  class="form-control" required placeholder="Nhập lại mật khẩu(*)">
+        <input type="password" name = "password2"data-validation-match-match="password"  class="form-control" data-validation-required-message="This field is required" placeholder="Nhập lại mật khẩu(*)">
         <span class="ion ion-log-in form-control-feedback "></span>
       </div>
       <div class="row">
