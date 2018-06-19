bca38772f099751b77272cca1dfc26e6dd22ceb8
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-19 13:12:58 +0700

    1

--- views/front/info.tpl.php
+++ views/front/info.tpl.php
@@ -206,14 +206,10 @@
                                                                                     <input type="password" name="password" placeholder="mật khẩu cũ" class="form-control" id="" value=""required> 
                                                                                 </div>
                                                                                 <label class="control-label mb-10" >Mật khẩu mới</label>
-                                                                                <div class="input-group">
-                                                                 
-                                                                                    <input type="password" name="password" placeholder="mật khẩu mới" class="form-control" id="first" value=""required> 
+                                                                                <input type="password" name="password" class="form-control" required data-validation-required-message="This field is required"minlength="6"  placeholder="nhập mật khẩu" > </div>
                                                                                 </div>
-                                                                                <label class="control-label mb-10" >Nhập lại mật khẩu mới</label>
-                                                                                <div class="input-group">
+                                                                                <input type="password" name="password2" data-validation-match-match="password" class="form-control"  placeholder="nhập lại mật khẩu"  required> </div>>
                                                                  
-                                                                                <input type="password" name="password" class="form-control" placeholder="nhập lại mật khẩu" id="second" value=""required> 
                                                                                  </div>
                                                                             </div>
                                                                             </div>
@@ -342,6 +338,17 @@ function validateNumber(elem) {
   }
 }
 </script>
+	<!-- Form validator JavaScript -->
+    <script src="../../admin/js/pages/validation.js"></script>
+    <script>
+    ! function(window, document, $) {
+        "use strict";
+        $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(), $(".skin-square input").iCheck({
+            checkboxClass: "icheckbox_square-green",
+            radioClass: "iradio_square-green"
+        }), $(".touchspin").TouchSpin(), $(".switchBootstrap").bootstrapSwitch();
+    }(window, document, jQuery);
+    </script>
 
     <!-- /script for show password -->
 
