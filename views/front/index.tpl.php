ae985cd31232b1908f5ce7c394d1cdf80abec055
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-26 12:49:22 +0700

    1

--- views/front/index.tpl.php
+++ views/front/index.tpl.php
@@ -623,7 +623,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://tawk.to/chat/5b31d0ffeba8cd3125e32bc5/default/?$_tawk_popout=true';
+s1.src='https://tawk.to/chat/5b31d0ffeba8cd3125e32bc5/default/';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
