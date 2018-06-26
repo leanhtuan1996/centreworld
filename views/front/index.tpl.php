266b79a947b026d7151829aed86b54028ea21f3f
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-26 12:29:54 +0700

    1

--- views/front/index.tpl.php
+++ views/front/index.tpl.php
@@ -630,7 +630,10 @@ s0.parentNode.insertBefore(s1,s0);
 		</script>
 	<!-- //PopUp-Box-JavaScript -->
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> parent of 94e64b5... 1
 
 >>>>>>> parent of 94e64b5... 1
 </body>
