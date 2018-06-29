7fd77201f4d68b27ef5410493f320a93eee6834f
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-29 11:19:02 +0700

    1

--- admin/cms/updatekiot.php
+++ admin/cms/updatekiot.php
@@ -129,7 +129,7 @@
              </div>
              </div>
              <div class="row">
-             <img style="" src="../<?php echo $product['image']?>"/>
+             <img style="" src="../<?php echo $kiot['image']?>"/>
                <div class="col-md-12">
                 <div class="form-group">
 						  <input type="file" value ="<?php echo $kiot['image']?>" name="fImg"class="form-control" required="" aria-invalid="false">
