120cc7a907ac4a2caf3dfd3184012dd0b48653bf
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-29 15:38:37 +0700

    1

--- admin/cms/updatekiot.php
+++ admin/cms/updatekiot.php
@@ -38,6 +38,10 @@
             // echo '</script>';
             echo "<script type='text/javascript'>alert('$image');</script>";	
             echo "<script type='text/javascript'>alert('$name_k');</script>";	
+            echo "<script type='text/javascript'>alert('$ID_k');</script>";	
+            echo "<script type='text/javascript'>alert('$price');</script>";	
+            echo "<script type='text/javascript'>alert('$status');</script>";	
+            echo "<script type='text/javascript'>alert('$description');</script>";	
           }
          ?>
         
