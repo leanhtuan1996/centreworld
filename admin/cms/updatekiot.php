1d89c03d1236eb336cc387ad4fde3659e19682f2
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-29 12:08:10 +0700

    1

--- admin/cms/updatekiot.php
+++ admin/cms/updatekiot.php
@@ -15,7 +15,7 @@
             if (empty($image_ori)){
               $error_add['image'] = 'Hình sản phẩm không được để trống';
           }else{
-              $image = "centreworld/customer/$image_ori";
+              $image = "../customer/$image_ori";
           }
           $kiot_edit = array(
             'name_k' => $name_k,
