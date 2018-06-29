36032d1672fa36f657689e9e493ba325327ba33f
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-06-29 15:19:09 +0700

    1

--- admin/cms/updatekiot.php
+++ admin/cms/updatekiot.php
@@ -30,7 +30,7 @@
             $error = db_kiot_validate($kiot_edit);
 
             if(!$error){
-              $error = editProduct($kiot_edit);
+              $error = editkiot($kiot_edit);
               move_uploaded_file($_FILES['fImg']['tmp_name'],"../$image");
           }
             // echo '<script language="javascript">';
