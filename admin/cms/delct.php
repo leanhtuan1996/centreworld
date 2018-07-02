033268d40e43d884eb935867db6bf98b4d6983f4
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-07-02 11:49:42 +0700

    1

--- admin/cms/delct.php
+++ admin/cms/delct.php
@@ -2,10 +2,10 @@
       include_once './models/users.php';
       include_once './models/kiot.php';
      $ID_CT = isset($_GET['ID_CT']) ? $_GET['ID_CT'] : '';
+     $ID_k = isset($_GET['ID_k']) ? $_GET['ID_k'] : '';
      if(!empty($ID_CT)){
           if(delct($ID_CT) > 0){
             //    redirect('index.php');
-            $kiot = getkiottById( $contract['ID_k']);
             editkiotbystatus1($ID_k);
             echo '<script language="javascript">';
             echo 'window.location.href = "./index.php"';
