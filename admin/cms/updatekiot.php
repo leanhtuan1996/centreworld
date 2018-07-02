a73053a004ff5b2e2f1558a675870202a31f1ae1
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-07-02 10:32:31 +0700

    1

--- admin/cms/updatekiot.php
+++ admin/cms/updatekiot.php
@@ -96,7 +96,7 @@
                    if ($kiot ['id_floor']==0)
                    {
                      ?>
-                   <option selected name ="id_floor" value="0">khu tổng hợp</option>;
+                   <option selected name ="id_floor" value="0">khu tổng hợp</option>
                    <option  name ="id_floor" value="1">khu mua sắm</option>
                    <option name ="id_floor" value="2">khu ăn uống</option>
                    <option name ="id_floor" value="3">khu vui chơi</option>
