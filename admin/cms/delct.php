c14c7bb71789bbc7071af09739f45d35f8f55693
trancaocuong96 <40144416+trancaocuong96@users.noreply.github.com>
2018-07-03 14:47:42 +0700

    1

--- admin/cms/delct.php
+++ admin/cms/delct.php
@@ -3,14 +3,15 @@
       include_once './models/kiot.php';
      $ID_CT = isset($_GET['ID_CT']) ? $_GET['ID_CT'] : '';
     $ID_k = getidkbyct($ID_CT);
-    echo "<script type='text/javascript'>alert('$ID_k');</script>";
+
      editkiotbyct($ID_k);
      if(!empty($ID_CT)){
+      echo "<script type='text/javascript'>alert('$ID_k');</script>";
           if(delct($ID_CT) > 0){
             //    redirect('index.php');
-            echo '<script language="javascript">';
-            echo 'window.location.href = "./index.php"';
-            echo '</script>';
+            // echo '<script language="javascript">';
+            // echo 'window.location.href = "./index.php"';
+            // echo '</script>';
           }
      }     
 ?>
